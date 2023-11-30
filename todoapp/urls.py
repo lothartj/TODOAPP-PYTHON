97ff0296ca6db49d8604c7e342dfaf26ddcbed1e
Lothar Tjipueja <lotharwebdev@gmail.com>
2023-11-30 12:07:15 +0200

    firstcommit

---
+++ todoapp/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.task_list, name='task-list'),
+    path('delete/', views.task_delete, name='delete-task')
+]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def task_list(request):
+    context = {}
+    return render(request, 'task_list.html', context)
+
+def task_update(request):
+    context = {}
+    return render(request, 'task_update.html', context)
+
+def task_delete(request):
+    context = {}
+    return render(request, 'task_delete.html', context)
+
